e0101f7f499256a2efe11dc4abe45bb6ca6dc366
Jake Carlson <jrcarlson@smu.edu>
2018-10-05 15:57:14 -0500

    Adds argument parser

---
+++ main.py
@@ -0,0 +1,22 @@
+import argparse
+
+def build_arg_parser():
+    parser = argparse.ArgumentParser(prog='survey-forge')
+    parser.add_argument('-n', '--num-respondents', type=int, default=20,
+                        help='Number of respondents to the survey', metavar='num')
+    parser.add_argument('-i', '--num-items', type=int, default=40,
+                        help='Number of items in the survey', metavar='num')
+    parser.add_argument('-s', '--scale', type=int, default=5,
+                        help='Number of points in the Likert scale for each item', metavar='num')
+    parser.add_argument('-a', '--coeff-alpha', type=float, default=0.80,
+                        help='Cronbach\'s alpha for the resulting survey data', metavar='float')
+    
+    return parser
+
+def main():
+    parser = build_arg_parser()
+    args = parser.parse_args()
+    print(args)
+
+if __name__ == '__main__':
+    main()
