cc46b8f78f1ea08d8639707c036b0c8cc27f2e43
Jake Carlson <jrcarlson@smu.edu>
2018-10-28 13:36:20 -0500

    Move alpha args to sub parser

--- main.py
+++ main.py
@@ -7,14 +7,17 @@ def build_arg_parser():
                         help='Number of respondents to the survey', metavar='num')
     parser.add_argument('-i', '--num-items', type=int, default=40,
                         help='Number of items in the survey', metavar='num')
-    parser.add_argument('-s', '--scale', type=int, default=5,
-                        help='Number of points in the Likert scale for each item', metavar='num')
-    parser.add_argument('-a', '--coeff-alpha', type=float, default=0.80,
-                        help='Cronbach\'s alpha for the resulting survey data', metavar='float')
-    parser.add_argument('-t', '--alpha-tol', type=float, default=0.05,
-                        help='Tolerance for Cronbach\'s alpha', metavar='float')
     parser.add_argument('-f', '--output-file', type=str, default="output.csv",
                         help='File to write final data set to', metavar='path')
+
+    sub_parsers = parser.add_subparsers()
+    alpha_parser = sub_parsers.add_parser('alpha')
+    alpha_parser.add_argument('-s', '--scale', type=int, default=5, metavar='num',
+                              help='Number of points in the Likert scale for each item')
+    alpha_parser.add_argument('-a', '--coeff-alpha', type=float, default=0.80, metavar='float',
+                              help='Cronbach\'s alpha for the resulting survey data')
+    alpha_parser.add_argument('-t', '--alpha-tol', type=float, default=0.05, metavar='float',
+                              help='Tolerance for Cronbach\'s alpha')
     
     return parser
 
